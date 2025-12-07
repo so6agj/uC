6ab40bbb47d4551654171298e319981447faacd1
so6agj <j.k.markowski@gmail.com>
2025-12-07 16:17:06 +0100

    Update nvm.c

--- avr/xmega_aprs_data/src/nvm.c
+++ avr/xmega_aprs_data/src/nvm.c
@@ -1,1 +1,18 @@
+#include <stdint.h>
+#include <avr/io.h>
+#include <stdio.h>
+#include <avr/pgmspace.h>
 
+#include "nvm.h"
+
+
+uint8_t nvm_read_byte(uint8_t command, uint8_t index)
+{
+    uint8_t result;
+
+    NVM.CMD = command;
+    result = pgm_read_byte(index);
+    NVM.CMD = NVM_CMD_NO_OPERATION_gc;
+
+    return result;
+}
